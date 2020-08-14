13990e495b9edf52112da13abb09005051f1ce54
Shaveyy <53417227+Shaveyy@users.noreply.github.com>
2020-08-14 00:02:35 -0400

    Spintax update

--- scripts/bot.py
+++ scripts/bot.py
@@ -25,14 +25,14 @@ class Bot:
         return s
 
     def SendMessage(self,message,account=None):
-        message = "PRIVMSG #{0} :{1}\r\n".format(self.channel,spintax.spintax.spin(message))
+        message = "PRIVMSG #{0} :{1}\r\n".format(self.channel,message)
         if(account):
             s = self.socks[account]
             s.send(message.encode())
         else:
             for s in self.socks:
                 try:
-                    s.send(message.encode())
+                    s.send(spintax.spintax.spin(message).encode())
                 except:
                     pass   
             
