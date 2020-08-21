889cc8e9a3abb638aa1d363ed12c4e9783b91175
Shaveyy <53417227+Shaveyy@users.noreply.github.com>
2020-08-21 02:58:32 -0400

    Make poll an embed

--- cogs/poll.py
+++ cogs/poll.py
@@ -6,6 +6,7 @@ class MiscCog(commands.Cog):
         self.bot = bot
     @commands.command()
     async def poll(self, ctx,arg):
+        embed = discord.Embed(title="Poll started by " + ctx.message.author.name,description=arg, color=0x00ff00)
         msg = await ctx.message.channel.send(arg)
         await ctx.message.delete()
         await msg.add_reaction('⬆️')
