8465bd2401af1c85d179d8787b54cfe185275e68
Shaveyy <53417227+Shaveyy@users.noreply.github.com>
2020-08-21 03:00:40 -0400

    forgot to send embed

--- cogs/poll.py
+++ cogs/poll.py
@@ -7,7 +7,7 @@ class MiscCog(commands.Cog):
     @commands.command()
     async def poll(self, ctx,arg):
         embed = discord.Embed(title="Poll started by " + ctx.message.author.name,description=arg, color=0x00ff00)
-        msg = await ctx.message.channel.send(arg)
+        msg = await ctx.message.channel.send(embed=embed)
         await ctx.message.delete()
         await msg.add_reaction('⬆️')
         await msg.add_reaction('⬇️')
